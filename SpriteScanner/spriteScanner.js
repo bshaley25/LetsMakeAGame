4351c166bc06e6fd3957d0708806afb9b5cf4dad
Bshaley25 <bshaley25@gmail.com>
2020-07-05 23:06:04 -0400

    fixing homepage

--- SpriteScanner/spriteScanner.js
+++ SpriteScanner/spriteScanner.js
@@ -1,5 +1,22 @@
-const canvas = document.createElement("canvas")
+const canvas = document.createElement('canvas')
+const w = canvas.width = 400
+const h = canvas.height = 400
+document.body.appendChild(canvas)
 const ctx = canvas.getContext('2d')
 
-document.body.appendChild(canvas)
+// const spriteSheet = new Image()
+// spriteSheet.src = './img/spaceShip.png'
+// document.body.appendChild(spriteSheet)
 
+ctx.arc(0,0,50,0, 2*Math.PI)
+
+
+// function animate() {
+//     ctx.beginPath()
+//     ctx.lineTo(50,50)
+//     ctx.stroke
+
+//     requestAnimationFrame(animate)
+// }
+
+// animate()
