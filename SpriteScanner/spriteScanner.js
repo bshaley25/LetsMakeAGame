d3335738734fc70d94aeafedd52e6cffc0c22004
Bshaley25 <bshaley25@gmail.com>
2020-07-12 22:06:13 -0400

    Added event listener for begining of game logic

--- SpriteScanner/spriteScanner.js
+++ SpriteScanner/spriteScanner.js
@@ -5,18 +5,26 @@ const h = canvas.height = 400
 const ctx = canvas.getContext("2d");
 
 const img = document.createElement("img")
-img.src = './img/spaceShip.png'
-document.body.appendChild(img)
+img.src = './img/spaceShip.png'; 
 
-document.body.appendChild(img)
+let frameX = 0
+let frameY = 0
+const spriteD = 60
 
 img.onload = function() {
     animate();
 };
 
+document.addEventListener("keydown", (event) => {
+    if(event.key === "ArrowRight") {
+        frameX++
+        console.log('yee')
+    }
+})
   
 function animate() {
-    ctx.drawImage(img, 0, 0, 60, 60, w*.35, h*.35, w/4, h/4)
+    // ctx.clearRect(0,0,w,h)
+    ctx.drawImage(img, frameX*spriteD, frameY*spriteD, frameX*spriteD + spriteD, frameY*spriteD + spriteD, w*.35, h*.35, w/4, h/4)
 
     requestAnimationFrame(animate())
 }
